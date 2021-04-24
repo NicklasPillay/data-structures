9b0b58faa031fab7334636603f76db6772118995
Nicklas Pillay <pillaynicklas@hotmail.com>
2021-04-24 21:21:36 +0200

    Initial commit for project

---
+++ Tutorials/SLL/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Tutorials {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
---
+++ Tutorials/StacksQueues/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace StacksQueues {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
